c697c37f0763a40789d7a0732881d9abf85750ea
Brett Wilkins <brett@brett.geek.nz>
2023-02-04 06:26:25 +0000

    new position for first root

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -4,6 +4,6 @@ import T1MB2LR from '../images/roots/T1MB2LR.png'
 
 export const Tree = () => (
     <Container>
-        <Sprite image={T1MB2LR} position={[1024/2 -32, 768/2 - 32]}/>
+        <Sprite image={T1MB2LR} position={[1024/2 -32, 768/2 + 32]}/>
     </Container>
 )
