40ee2536468f7c63eb261eac3cfb60a976bfa0e4
Brett Wilkins <brett@brett.geek.nz>
2023-02-04 06:28:41 +0000

    Add Tree container position

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -3,7 +3,7 @@ import { Container, Sprite } from '@pixi/react'
 import T1MB2LR from '../images/roots/T1MB2LR.png'
 
 export const Tree = () => (
-    <Container>
+    <Container position={[0, 0]}>
         <Sprite image={T1MB2LR} position={[1024/2 -32, 768/2 + 32]}/>
     </Container>
 )
