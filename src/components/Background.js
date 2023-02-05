1119e0182a3e456409f9cfe6481e689284387a9b
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 10:06:30 +0000

    set explicit width and height for background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,10 @@
 import { Sprite, Texture } from 'pixijs'
+import { screenDimensions } from '../config/config'
 
 import background from '../images/background.png'
 
 export const BackgroundTexture = Texture.from(background)
 export const Background = new Sprite(BackgroundTexture)
 Background.position = [0, 0]
+Background.width = screenDimensions.width
+Background.height = screenDimensions.height
