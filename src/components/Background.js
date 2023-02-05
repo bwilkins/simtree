5210fae0d2fdc392f8a500535000691e38cf4208
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 09:55:45 +0000

    does the background need a position?

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,3 +4,4 @@ import background from '../images/background.png'
 
 export const BackgroundTexture = Texture.from(background)
 export const Background = new Sprite(BackgroundTexture)
+Background.position = [0, 0]
