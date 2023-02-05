4e03f85002a885fd4fc3b9e9e255ecbbff3091ff
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 08:42:51 +0000

    shouldn't need to explicitly set true, but try it anyway

--- src/components/GrowButton.js
+++ src/components/GrowButton.js
@@ -1,5 +1,5 @@
 import { Sprite } from '@pixi/react'
 import GrowButton_image from '../images/button-grow.png'
 
-export const GrowButton = ({onClick = () => {}}) =>
-    <Sprite image={GrowButton_image} interactive onClick={onClick} />
+export const GrowButton = ({onClick}) =>
+    <Sprite image={GrowButton_image} interactive={true} onClick={onClick} />
