3a07f8038ed67a3b2df0f13dbcc7a329edc7edbc
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 11:42:34 +0000

    try adding containers to stage before filling them

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -10,24 +10,24 @@ export const Game = (app) => {
     const gameStage = app.stage;
 
     const backgroundContainer = new Container()
+    gameStage.addChild(backgroundContainer)
     backgroundContainer.position = [0, 0]
     backgroundContainer.weight = 1024
     backgroundContainer.height = 768
     backgroundContainer.addChild(Background)
-    gameStage.addChild(backgroundContainer)
     console.log('background added')
 
     const treeContainer = new Container()
+    gameStage.addChild(treeContainer)
     treeContainer.position = [0, 0]
     treeContainer.weight = 1024
     treeContainer.height = 768
     treeContainer.addChild(Tree)
-    gameStage.addChild(treeContainer)
     console.log('tree added')
 
     const menuContainer = new Container()
+    gameStage.addChild(menuContainer)
     menuContainer.position = [screenDimensions.width - 192, 0]
     menuContainer.addChild(Menu)
-    gameStage.addChild(menuContainer)
     console.log('menu added')
 }
