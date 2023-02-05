c50efc73ae1faac896ae25ee3c0f6e35e82e8db7
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 22:21:30 +0000

    load images before loading game object?

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import * as PIXI from 'pixijs'
 import { PixiApp } from './containers/PixiApp';
+import './images'
 import { Game } from './containers/Game'
 
 window.PIXI = PIXI
@@ -10,7 +11,9 @@ window.PixiApp = PixiApp
 // Create the application helper and add its render target to the page
 document.body.appendChild(PixiApp.view);
 
-Game(PixiApp)
+await PIXI.Assets.load(['background', 'T1MB1M', 'T1MR1M', 'T1ML1M', 'T1MB2LR', 'T2LRB1M', 'L1MB1M', 'R1MB1M', 'growButton'])
+
+await Game(PixiApp)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
