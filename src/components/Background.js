68b65a2c5ae14baf0f18dede75fe7d700b787aff
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 22:23:19 +0000

    use asset again

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,7 @@
 import { Assets, Sprite } from 'pixijs'
-import {background} from '../images'
+import '../images'
 
-// export const BackgroundTexture = await Assets.load('background')
-export const Background = Sprite.from(background)
+export const BackgroundTexture = await Assets.load('background')
+export const Background = Sprite.from(BackgroundTexture)
 
 console.log("Background", Background);
