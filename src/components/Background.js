88d235ef5f5566a70191180cf0989b1a05028c68
Brett Wilkins <brett@brett.geek.nz>
2023-02-05 12:08:31 +0000

    use from for direct URL

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,6 +2,6 @@ import { Assets, Sprite } from 'pixijs'
 import {background} from '../images'
 
 // export const BackgroundTexture = await Assets.load('background')
-export const Background = new Sprite(background)
+export const Background = Sprite.from(background)
 
 console.log("Background", Background);
